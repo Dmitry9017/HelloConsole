205dd8147c2c08efdb0b0af046e595235b4060dc
Dmitry <dima_gazavto@mail.ru>
2022-08-12 13:44:59 +0300

    IFelse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+    if(userName.ToLower() == "юля"){
+        Console.WriteLine("Категорически приветствую Вас, " + userName);
+    } else {
+        Console.WriteLine("Привет, " + userName + "!");
+    }
+
